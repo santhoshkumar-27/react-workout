bf3e69777debfb3e1d72bb4dae34903b3f5c5448
santhoshkumar <its.santhoshkumar@outlook.com>
2024-10-18 11:05:38 +0530

    feat: max length

--- 03-react-essentials/src/App.jsx
+++ 03-react-essentials/src/App.jsx
@@ -1,10 +1,18 @@
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+
+
 function Header() {
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {reactDescriptions[genRandomInt(reactDescriptions.length - 1)]} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
